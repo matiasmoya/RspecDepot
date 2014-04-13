3eefa50f5057d77e50e8d138150754b334cdf149
Matt Moya <matiasmoya@gmail.com>
2014-04-13 14:41:32 -0300

    Habilitado envio de correos para confirmar ordenes y envios

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -36,6 +36,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
+        OrderNotifier.received(@order).deliver
         format.html { redirect_to store_url, notice: 'Gracias por su compra!' }
         format.json { render action: 'show', status: :created, location: @order }
       else

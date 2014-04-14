b029fb7bcdf59ee5bb41532da7f4f2d48770ca33
Matt Moya <matiasmoya@gmail.com>
2014-04-14 16:05:45 -0300

    Agregado soporte de control de sesiones y modo administrador

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,8 @@
 class StoreController < ApplicationController
-  include CurrentCart
-  before_action :set_cart
-  def index
-  	@products = Product.order(:title)
-  end
+	skip_before_action :authorize
+	  include CurrentCart
+	  before_action :set_cart
+	  def index
+	  	@products = Product.order(:title)
+	  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.order(:name)
   end
 
   # GET /users/1
@@ -25,10 +25,10 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
+    
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to users_url, notice: "User #{@user.name} was successfully created." }
         format.json { render action: 'show', status: :created, location: @user }
       else
         format.html { render action: 'new' }
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to users_url, notice: "User #{@user.name} was successfully updated." }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -54,7 +54,12 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
+    begin
     @user.destroy
+    flash[:notice] = "User #{@user.name} deleted"
+  rescue StandardError => e
+    flash[:notice] = e.message
+    end
     respond_to do |format|
       format.html { redirect_to users_url }
       format.json { head :no_content }

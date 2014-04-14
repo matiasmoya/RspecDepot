a693176ee0f08b56bcba6a3d447536a389034861
Matt Moya <matiasmoya@gmail.com>
2014-04-14 17:16:54 -0300

    Agregado soporte para intenacionalizacion, eng-esp

--- config/routes.rb
+++ config/routes.rb
@@ -9,21 +9,19 @@ end
   # get "sessions/create"
   # get "sessions/destroy"
   resources :users
-
-  resources :orders
-
-  resources :line_items
-
-  resources :carts
-
   resources :products
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'store#index'
-   get 'store/index', :as => 'store'
+   # get 'store/index', :as => 'store'
  
    resources :products do
       get :who_bought, on: :member

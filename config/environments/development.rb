9ccf9462e6120da18fdc4f1caa43ff3c5ee1837c
Matt Moya <matiasmoya@gmail.com>
2014-04-16 14:29:51 -0300

    Fixing checkouts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,17 +14,17 @@ DepotA::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.delivery_method = :test
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.smtp_settings = {
-    address:"smtp.gmail.com",
-    port:587,
-    domain:"gmail.com",
-    authentication: "plain",
-    user_name:"hinczuk",
-    password:"31323314",
-    enable_starttls_auto: true
-}
+#   config.action_mailer.delivery_method = :test
+#   config.action_mailer.raise_delivery_errors = false
+#   config.action_mailer.smtp_settings = {
+#     address:"smtp.gmail.com",
+#     port:587,
+#     domain:"gmail.com",
+#     authentication: "plain",
+#     user_name:"hinczuk",
+#     password:"31323314",
+#     enable_starttls_auto: true
+# }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
